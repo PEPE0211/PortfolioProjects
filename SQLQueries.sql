798a90c4d01b8d5db6ae27d6d5e111d9ce1063ae
Mihir Panchal <61843177+PEPE0211@users.noreply.github.com>
2023-10-13 18:23:29 -0400

    Add files via upload

---
+++ SQLQueries.sql
@@ -0,0 +1,88 @@
+select *
+From PortfolioProject..CovidDeaths
+order by 3,4
+
+--select *
+--From PortfolioProject..CovidVaccinations
+--order by 3,4
+
+select Location,Date , total_cases,new_cases,total_deaths,population
+From PortfolioProject..CovidDeaths
+Where continent is not null
+order by 1,2
+
+-- Total Cases vs Total deaths
+select Location,Date , total_cases,total_deaths, (CONVERT(int, total_deaths) / NULLIF(CONVERT(float, total_cases), 0))*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
+Where location like '%Canada%'
+order by 1,2
+
+-- Total Cases vs the Population
+select Location,Date ,population, total_cases, (CONVERT(float, total_cases) /NULLIF(CONVERT(float, population), 0))*100 as CasePercentage
+From PortfolioProject..CovidDeaths
+Where location like '%Afghanistan%'
+order by 1,2
+
+select Location,population, MAX(CONVERT(float, total_cases)) as HighestCases, MAX(CONVERT(float, total_cases) /NULLIF(CONVERT(float, population), 0))*100 as CasePercentage
+From PortfolioProject..CovidDeaths
+Where continent is not null
+Group by Location,population
+order by CasePercentage desc
+
+select Location, MAX(CONVERT(int, total_deaths)) as HighestDeathCounrt
+From PortfolioProject..CovidDeaths
+Where continent is not null
+Group by Location
+order by HighestDeathCounrt desc
+
+select date ,SUM(new_cases) as GlobalTotalCases, SUM(CONVERT(int, new_deaths)) as GlobalTotalDeaths, SUM(CONVERT(int, new_deaths)) / SUM(NULLIF(CONVERT(float, new_cases),0))*100 as GlobalDeathPercentage
+From PortfolioProject..CovidDeaths
+Where continent is not null
+Group by date
+order by 1,2
+
+select SUM(new_cases) as GlobalTotalCases, SUM(CONVERT(int, new_deaths)) as GlobalTotalDeaths, SUM(CONVERT(int, new_deaths)) / SUM(NULLIF(CONVERT(float, new_cases),0))*100 as GlobalDeathPercentage
+From PortfolioProject..CovidDeaths
+Where continent is not null
+order by 1,2
+
+Select * 
+From PortfolioProject..CovidDeaths dea
+Join PortfolioProject..CovidVaccinations vac
+	ON dea.location = vac.location
+	and dea.date = vac.date
+
+Select dea.continent,dea.location, dea.date,dea.population,vac.new_vaccinations, SUM(CONVERT(bigint,vac.new_vaccinations)) OVER (Partition by dea.location Order by dea.location,dea.date) as TotalVaccinationsEveryDate
+From PortfolioProject..CovidDeaths dea
+Join PortfolioProject..CovidVaccinations vac
+	ON dea.location = vac.location
+	and dea.date = vac.date
+WHERE dea.continent is not null
+and vac.new_vaccinations is not null
+Order by 1,2
+
+With PopVsVac(Continent, location, date,population, new_vaccinations, TotalVaccinationsEveryDate)
+as
+(
+Select dea.continent,dea.location, dea.date,dea.population,vac.new_vaccinations, SUM(CONVERT(bigint,vac.new_vaccinations)) OVER (Partition by dea.location Order by dea.location,dea.date) as TotalVaccinationsEveryDate
+From PortfolioProject..CovidDeaths dea
+Join PortfolioProject..CovidVaccinations vac
+	ON dea.location = vac.location
+	and dea.date = vac.date
+WHERE dea.continent is not null
+and vac.new_vaccinations is not null
+)
+Select *, (TotalVaccinationsEveryDate/population)*100 as TotalVaccinationPercentage
+from PopVsVac
+
+Create View PercentageOfVaccinations as
+Select dea.continent,dea.location, dea.date,dea.population,vac.new_vaccinations, SUM(CONVERT(bigint,vac.new_vaccinations)) OVER (Partition by dea.location Order by dea.location,dea.date) as TotalVaccinationsEveryDate
+From PortfolioProject..CovidDeaths dea
+Join PortfolioProject..CovidVaccinations vac
+	ON dea.location = vac.location
+	and dea.date = vac.date
+WHERE dea.continent is not null
+and vac.new_vaccinations is not null
+
+Select * 
+From PercentageOfVaccinations
